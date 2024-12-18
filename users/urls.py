a2f86cbbefa8cf1e55af0c54ec4ba553976eb94a
AI-ML-Prowork <pythoncfc@gmail.com>
2024-12-18 14:50:09 +0530

    added

--- users/urls.py
+++ users/urls.py
@@ -108,7 +108,7 @@ schema_view = get_schema_view(
 urlpatterns = [
     # for the super admin(saas login)
     path("admin", admin.site.urls),
-    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/superadmin-login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('users', CustomUserListView.as_view(), name='custom-user-list'),
     path('domains', DomainListView.as_view(), name='domain-list'),
